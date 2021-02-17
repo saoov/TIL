d4ca9d36022cff504a2a5fdc3a230243f1b0b3b6
saoov <saoov0927@naver.com>
2021-02-17 10:39:24 +0900

    thymeleaf template

--- ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex3.dto.SampleDTO;
 
 import java.time.LocalDateTime;
@@ -18,21 +19,45 @@ import java.util.stream.IntStream;
 @Log4j2
 public class SampleController {
     @GetMapping("ex1")
-    public void ex1(){
+    public void ex1() {
         log.info("ex1~~~~~~~~~~~");
     }
 
-    @GetMapping({"ex2"})
+    @GetMapping({"ex2", "/exLink"})
     public void exModel(Model model) {
-        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->{
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
             SampleDTO dto = SampleDTO.builder()
                     .sno(i)
                     .first("first....." + i)
-                    .last("last..."+i)
+                    .last("last..." + i)
                     .regTime(LocalDateTime.now())
                     .build();
             return dto;
         }).collect(Collectors.toList());
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline..........");
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("first.....100")
+                .last("last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("ex3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    @GetMapping({"/exLayout1","/exLayout2", "/exTemplate","/exSidebar"})
+    public void exLayout1(){
+        log.info("exLayout~~~~~~~~~~~~~~~");
     }
 }
